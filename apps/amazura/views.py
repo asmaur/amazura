da990c7fd0a4ea3cd0bff7609b2dc8f4e6dfe17a
asmaur <maur.a@outlook.com>
2018-11-26 21:09:26 -0200

    errors

--- apps/amazura/views.py
+++ apps/amazura/views.py
@@ -6,9 +6,9 @@ from django.conf import settings
 
 def set_cookies(response, key, value, days_expire = 30):
     if days_expire is None:
-        max_age = 30 #365 * 24 * 60 * 60  # one year
+        max_age = 365 * 24 * 60 * 60  # one year
     else:
-        max_age = days_expire #* 24 * 60 * 60
+        max_age = days_expire * 24 * 60 * 60
     expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age),
                                          "%a, %d-%b-%Y %H:%M:%S GMT")
     response.set_cookie(key, value, max_age=max_age, expires=expires, domain=settings.SESSION_COOKIE_DOMAIN,
