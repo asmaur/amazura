025dfdd81ebf4093657b2c346ccbb873ebad3751
asmaur <maur.a@outlook.com>
2018-11-27 04:08:03 -0200

    corrigir staticfiles

---
+++ static/js/loader.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function() {
+    //Preloader
+    $(window).on("load", function() {
+    preloaderFadeOutTime = 500;
+    function hidePreloader() {
+    var preloader = $('#page-loader');
+    preloader.fadeOut(preloaderFadeOutTime);
+    }
+    hidePreloader();
+    });
+});
+
