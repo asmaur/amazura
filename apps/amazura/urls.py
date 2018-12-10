9174245e3abdc90631b2c15c3867bec2ae95bf9e
asmaur <maur.a@outlook.com>
2018-12-10 08:54:25 -0200

    new version

--- apps/amazura/urls.py
+++ apps/amazura/urls.py
@@ -6,9 +6,9 @@ from .views import *
 
 urlpatterns = [
     re_path(r'^$',init),
-    re_path(r'^inicio',index),
+    re_path(r'^inicio$',index),
     re_path(r'^contato$',contato),
-    re_path(r'^valores',valores),
-    re_path(r'^photos/(?P<slug>[\w-]+)/(?P<id>\d+)',page),
+    re_path(r'^valores$',valores),
+    re_path(r'^photos/(?P<slug>[\w-]+)/(?P<id>\d+)$',page),
 
 ]
